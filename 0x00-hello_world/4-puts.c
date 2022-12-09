b4f5b36b1236d4a0eff73fe1ebd7db15c8b6acc1
NatanYirg <natiyirgish@gmail.com>
2022-12-08 16:02:43 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - main block
  * Return: o
  */
-int main (void)
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
