5966ef1449af3848fc76baf92c1fcd131fabe2f4
NatanYirg <natiyirgish@gmail.com>
2022-12-08 16:17:56 -0800

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * return: 0
+ */
+  
+int main(void)                                                                                                   
+{                                                                                                                
+	        printf("with proper grammer, but the oytcome is piece of art,\n");                                            
+		        return (0);                                                                                              
+} 
