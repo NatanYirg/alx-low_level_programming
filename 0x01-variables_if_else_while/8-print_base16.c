a04dcf519abb2d9657edcaec235d18f9e7df4aaa
NatanYirg <natiyirgish@gmail.com>
2022-12-09 15:31:40 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print letters
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
