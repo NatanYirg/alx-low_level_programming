be44480353cf74caf9fc076d22e0e75fa1f8255c
NatanYirg <natiyirgish@gmail.com>
2022-12-13 14:48:30 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @t - Integer to be computed
+ * Return: The absoute vaue
+ */
+
+int _abs(int)
+{
+	if (t >= 0)
+		return (1);
+	else
+		return (-1);
+}
