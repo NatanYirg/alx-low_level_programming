e96a78c2d304e274085c4cc717f3e340d4e75358
NatanYirg <natiyirgish@gmail.com>
2022-12-13 15:48:54 -0800

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - print the nine tables
+ */
+
+void void times_table(void)
+{
+	int num, mul, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mul = 0; mul <= 9; mul++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mul;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
