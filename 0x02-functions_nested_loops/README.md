3fa7119943f7ad94fa3e4c3ad9bc158074d4a3bd
NatanYirg <natiyirgish@gmail.com>
2022-12-13 07:41:18 -0800

    mainh

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+functions and nested loops > README.md
+
+q
+
+  
+echo functions and nested loops
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char c)
+
+
+
+
+
+
+
+
+
+
+
+
+#endif 
