d5e7cdcd833791f588200e933a451d25fa93b29f
NatanYirg <natiyirgish@gmail.com>
2022-12-13 16:54:33 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,8 +17,9 @@ void print_times_table(int n)
 	{
 		for (b = 0; b <= n; b++)
 		{
-			rep = a * b; if (b == 0)
-			_putchar('0' + rep);
+			rep = a * b; 
+			if (b == 0)
+				_putchar('0' + rep);
 			else if (rep < 10)
 			{
 				_putchar(' ');
