98b05106ec0a99f0634d54f9fc93b63e3e7b249b
NatanYirg <natiyirgish@gmail.com>
2022-12-15 09:49:19 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != '2' && i != '4')
 		{
 			_putchar(i);
 		}
