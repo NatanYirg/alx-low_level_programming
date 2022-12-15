db29e21482decfaf4659417c2f51765a96083373
NatanYirg <natiyirgish@gmail.com>
2022-12-15 05:26:25 -0800

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if parameter is uppercase
+ * @c: input character
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c == 'C')
+		return (1);
+	else
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+
+int _putchar(char);
+int _isupper(int c);
+
+
+
+
+#endif
