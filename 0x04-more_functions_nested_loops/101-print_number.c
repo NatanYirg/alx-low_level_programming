2b29204135991a57b80495c31f22cddab5b0066a
NatanYirg <natiyirgish@gmail.com>
2022-12-15 12:15:10 -0800

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: input integer
- * Return: no return
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 
 void print_number(int n)
@@ -19,6 +19,7 @@ void print_number(int n)
 	{
 		m = n;
 	}
+
 	d = m;
 	count = 1;
 
