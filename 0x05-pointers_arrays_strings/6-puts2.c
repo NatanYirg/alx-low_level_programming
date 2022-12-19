d026fea4eb94f57dee54d1187c53b181b2dd46d4
NatanYirg <natiyirgish@gmail.com>
2022-12-19 13:23:32 -0800

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints a string followed by a new line
+ * puts2 - prints a string, followed by a new line,
  * @str: pointer to the string to print
  * Return: void
  */
