72d37bbdd74b3b91460880d1503b64f84906ebc1
NatanYirg <natiyirgish@gmail.com>
2022-12-19 14:29:51 -0800

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,13 @@ int _atoi(char *s)
 	int n = 0;
 	int signo = 1;
 
-	while ((s[i] < '0' || s[i] > '9' && s[i] != 0)
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
 		if (s[i] == '-')
 			signo *= -1;
 		i++;
 	}
-	while ((s[i] >= '0' && s[i] <= '9' && s[i] != 0)
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 		{
 		if (n >= 0)
 		{
