f422419fc68f8df7e3f4784864169e6f581fb915
NatanYirg <natiyirgish@gmail.com>
2022-12-19 15:06:53 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 	num = 0;
 	sign = 1;
 
-	while (*temp != '0' && (*temp < '0' || *temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '-')
 			sign *= -1;
