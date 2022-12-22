71a4f0d992df69b35452c3be92c6d6f827539579
NatanYirg <natiyirgish@gmail.com>
2022-12-22 14:18:06 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *leet(char *);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif
