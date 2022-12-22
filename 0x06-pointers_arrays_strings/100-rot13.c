99c7587b3bcd1237f60c3bb2960efcb71468111d
NatanYirg <natiyirgish@gmail.com>
2022-12-22 13:38:49 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,28 +8,28 @@
 
 char *rot13(char *str)
 {
-	int index1, index2;
+	int index1 = 0, index2;
 
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-											 'G', 'H', 'I', 'J', 'K', 'L',
-											 'M', 'N', 'O', 'P', 'Q', 'R',
-											 'S', 'T', 'U', 'V', 'W', 'X',
-											 'Y', 'Z', 'a', 'b', 'c', 'd',
-											 'e', 'f', 'g', 'h', 'i', 'j',
-											 'k', 'l', 'm', 'n', 'o', 'p',
-											 'q', 'r', 's', 't', 'u', 'v',
-											 'w', 'x', 'y', 'z'};
+			'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R',
+			'S', 'T', 'U', 'V', 'W', 'X',
+			'Y', 'Z', 'a', 'b', 'c', 'd',
+			'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm', 'n', 'o', 'p',
+			'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z'};
 
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-											 'T', 'U', 'V', 'W', 'X', 'Y',
-											 'Z', 'A', 'B', 'C', 'D', 'E',
-											 'F', 'G', 'H', 'I', 'J', 'K',
-											 'L', 'M', 'n', 'o', 'p', 'q',
-											 'r', 's', 't', 'u', 'v', 'w',
-											 'x', 'y', 'z', 'a', 'b', 'c',
-											 'd', 'e', 'f', 'g', 'h', 'i',
-											 'j', 'k', 'l', 'm'};
-	while (str[++index1])
+			'T', 'U', 'V', 'W', 'X', 'Y',
+			'Z', 'A', 'B', 'C', 'D', 'E',
+			'F', 'G', 'H', 'I', 'J', 'K',
+			'L', 'M', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w',
+			'x', 'y', 'z', 'a', 'b', 'c',
+			'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm'};
+	while (str[index1])
 	{
 		for (index2 = 0; index2 < 52; index2++)
 		{
@@ -39,6 +39,7 @@ char *rot13(char *str)
 				break;
 			}
 		}
+		index1++;
 	}
 	return (str);
 }
