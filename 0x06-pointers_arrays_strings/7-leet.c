101df1f7bda37bded45eae7a622a2810e66132d4
NatanYirg <natiyirgish@gmail.com>
2022-12-22 13:25:30 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *str)
 	int index1 = 0, index2;
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[++index1])
+	while (str[index1])
 	{
 		for (index2 = 0; index2 <= 7; index2++)
 		{
@@ -18,6 +18,8 @@ char *leet(char *str)
 			 str[index1] - 32 == leet[index2])
 				str[index1] = index2 + '0';
 		}
+
+		index1++;
 	}
 	return (str);
 }
