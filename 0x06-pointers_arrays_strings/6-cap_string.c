b46cfd99a54f9893457662158e5275d82f2cbf85
NatanYirg <natiyirgish@gmail.com>
2022-12-22 13:18:42 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[++index])
+	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
@@ -25,8 +25,11 @@ char *cap_string(char *str)
 				str[index - 1] == '(' ||
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
-				str[index - 1] == '}')
+				str[index - 1] == '}' ||
+				index == 0)
 			str[index] -= 32;
+
+		index++;
 	}
 	return (str);
 }
