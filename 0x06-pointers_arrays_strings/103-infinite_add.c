6f7b1cb595ca27c5c50b1c5d565481f863738afa
NatanYirg <natiyirgish@gmail.com>
2022-12-22 12:59:06 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -45,16 +45,16 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 	for (; *n1; n1--; r_index++)
 	{
-		num = *(n1 - '0') + tens; 
+		num = *(n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
 	for (; *n2; n2--;  r_index--)
 	{
-		num = (*n2 - '0') + tens; 
+		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10; 
+		tens = num / 10;
 	}
-	
+
 }
