0402461e25dd037db77b6c4c76898c89920076c4
NatanYirg <natiyirgish@gmail.com>
2023-01-16 12:03:30 -0800

    dog1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef "DOG_H"
-#define "DOG_H"
+#include <stdio.h>
+#include "main.h"
 
 /**
  * struct dog - Struct dog
@@ -15,12 +15,4 @@ struct dog
 	float age;
 	char *owner;
 };
-
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
 
