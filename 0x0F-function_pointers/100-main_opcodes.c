9fe5aba1e56a6d6fd14e9029f16ce86ea456245f
NatanYirg <natiyirgish@gmail.com>
2023-01-18 12:50:10 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,20 +1,35 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - ...
-* @argc: #arguments
-* @argv: arguments
-*
-* Return: 0
-*/
-int main(int argc, char **argv)
+ * main - check the code for Holberton School students.
+ * @argc: amount of args
+ * @argv: arguments
+ * Return: 1 if an error occurred, 0 otherwise
+ */
+int main(int argc, char *argv[])
 {
-	int i, b;
+	unsigned char *p = (unsigned char *)main;
+
+	unsigned int bytes;
+
+	int i;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	b = atoi(argv[1]);
-	if (b < 0)
-		printf("Error\n"), exit(2);
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	bytes = atoi(argv[1]);
+
+	for (i = 0; i < bytes; i++)
+		printf("%02x ", *p++);
+	printf("\n");
 	return (0);
 }
